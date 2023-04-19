f6a702f0f17a95f5b317e9641413aed8c7a6c812
serhasHacizade <eren.felek.4@gmail.com>
2023-04-19 14:25:32 +0300

    commit

--- project_2/src/app.js
+++ project_2/src/app.js
@@ -3,12 +3,18 @@ const app = express();
 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
+app.use(express.static("node_modules"));
 
 
 const data = [
-    {id : 1, name : "iphone 14", price : 3000, imageUrl : "../public/images/1.jpg"},
-    {id : 2, name : "Samsung s22", price : 2000, imageUrl : "../public/images/2.jpg"},
-    {id : 3, name : "iphone 11", price : 1000, imageUrl : "../public/images/3.jpg"}
+    {
+        id: 1,
+        name: "iphone 14", 
+        price: 3000, 
+        imageUrl: "https://www.teknotec.com.tr/image/cache/catalog/urunler/apple/iphone-14-128-gb/kirmizi/iphone-14-kirmizi-min-1000x1000.png"
+    },
+    { id: 2, name: "Samsung s22", price: 2000, imageUrl: "https://cdn.vatanbilgisayar.com/Upload/PRODUCT/samsung/thumb/132360-5_large.jpg" },
+    { id: 3, name: "iphone 11", price: 1000, imageUrl: "https://cdn.vatanbilgisayar.com/Upload/PRODUCT/apple/thumb/113155-1-1_large.jpg" }
 ];
 
 app.use("/products/:id", (req, res) => {
@@ -16,11 +22,11 @@ app.use("/products/:id", (req, res) => {
     res.render("productDetail", item);
 });
 app.use("/products", (req, res) => {
-    res.render("products", {data : data});
+    res.render("products", { data: data });
 });
 
 app.use("/", (req, res) => {
-	res.render("index");
+    res.render("index");
 });
 
 
