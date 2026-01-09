6437407479bdf977d1d774c61f3806f47e85e881
serhasHacizade <eren.felek.4@gmail.com>
2026-01-10 01:23:05 +0300

    commit

--- 1-create-server/app.js
+++ 1-create-server/app.js
@@ -1,7 +1,31 @@
-const http = require('http');
+const http = require("http");
 
 const server = http.createServer((req, res) => {
-    console.log(req);
+  console.log(req);
+
+  const url = req.url;
+ const method = req.method;
+
+  if (url === "/") {
+    res.write("<html>");
+    res.write("<head><title>Enter Message</title></&title>");
+    res.write(
+      "<body><form action='/message' method='POST'><input type='text' name='message'><button type='submit'>Send</button></form></body>"
+    );
+    res.write("</html>");
+    return res.end();
+  }
+  if (url === "/message" && method === "POST") {
+    
+  }
+
+  res.setHeader("Content-Type", "text/html");
+  res.write("<html>");
+  res.write("<head><title>My First Page</title></&title>");
+  res.write("<body><h1>Hello from my Node.js Server!</h1></body>");
+  res.write("</html>");
+  res.end();
+  //sending request
 });
 
 server.listen(3000);
