b348b06f6fb0f0f13be486bd088e2f89793a4d59
serhasHacizade <eren.felek.4@gmail.com>
2026-01-26 14:34:38 +0300

    commit

---
+++ 3-express.js/app.js
@@ -0,0 +1,31 @@
+const http = require('http');
+const express = require('express');
+
+const routes = require('./routes');
+const bodyParser = require('body-parser');
+
+const app = express();
+// app.use((req, res, next) => {
+//     console.log("in the middleware!");
+//     next();
+// });
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use("/add-product", (req, res, next) => {
+    console.log("in the middleware!");
+    res.send("<form action='/product' method='POST'><input type='text' name='title'/><button type='submit'>Add Product</button></form>");
+});
+
+app.use("/product", (req, res, next) => {
+    console.log(req.body);
+    
+    res.redirect("/");
+});
+app.use((req, res, next) => {
+    res.send("<h1>Hello from Express!</h1>");
+});
+const server = http.createServer(app);
+
+server.listen(3000);
+ 
