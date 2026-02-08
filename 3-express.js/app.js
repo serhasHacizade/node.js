587c16e26e2e8845bbd4d1978ab88772906f9187
serhasHacizade <eren.felek.4@gmail.com>
2026-02-08 12:46:55 +0300

    commit

--- 3-express.js/app.js
+++ 3-express.js/app.js
@@ -1,31 +1,20 @@
-const http = require('http');
-const express = require('express');
+const path = require('path');
 
-const routes = require('./routes');
+const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
-// app.use((req, res, next) => {
-//     console.log("in the middleware!");
-//     next();
-// });
 
-app.use(bodyParser.urlencoded({ extended: false }));
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
-app.use("/add-product", (req, res, next) => {
-    console.log("in the middleware!");
-    res.send("<form action='/product' method='POST'><input type='text' name='title'/><button type='submit'>Add Product</button></form>");
-});
+app.use(bodyParser.urlencoded({extended: false}));
 
-app.use("/product", (req, res, next) => {
-    console.log(req.body);
-    
-    res.redirect("/");
-});
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+
 app.use((req, res, next) => {
-    res.send("<h1>Hello from Express!</h1>");
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
-const server = http.createServer(app);
 
-server.listen(3000);
- 
+app.listen(3000);
